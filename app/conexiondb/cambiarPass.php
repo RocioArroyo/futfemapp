4818f86264b1b9b0ebf7f0f80b0abbcfc1a0664d
RocioArroyo <rocio.arroyo@outlook.com>
2020-06-05 17:21:42 +0200

    -- Corregidas las interfaces para mayor movilidad

--- app/conexiondb/cambiarPass.php
+++ app/conexiondb/cambiarPass.php
@@ -3,7 +3,7 @@ require "conn.php";
 $user_name= $_REQUEST['username'];
 $user_pass= $_REQUEST['password'];
 $consulta="SELECT * FROM USUARIO WHERE usu_email='$user_name'";
-$comprobar=mysqli_query($conn, $comprobar);
+$comprobar=mysqli_query($conn, $consulta);
 if (mysqli_num_rows($comprobar)==1) {
 	$mysql_query="UPDATE USUARIO SET usu_password='$user_pass' WHERE usu_email='$user_name'";
 	$result=mysqli_query($conn,$mysql_query);
